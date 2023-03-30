1032d3008e726210890c19dce0821f23b876d0b0
jnk1m <jn7277@hotmail.com>
2023-03-30 14:44:16 +0900

    Create Option

---
+++ ohouse/src/main/java/com/ohouse/ohouse/entity/Option.java
@@ -0,0 +1,30 @@
+package com.ohouse.ohouse.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "options")
+@Getter
+@NoArgsConstructor
+public class Option {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "option_id")
+  private Long optionId;
+
+  @Column(name = "option_name")
+  private String name;
+
+  @Column(name = "option_price")
+  private BigDecimal optionPrice;
+
+  public Option(String name, BigDecimal optionPrice) {
+    this.name = name;
+    this.optionPrice = optionPrice;
+  }
+}
