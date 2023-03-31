6cb6fe614494b9e585a6e2c8352902b51d8dbdb2
jnk1m <jn7277@hotmail.com>
2023-03-31 23:19:25 +0900

    Delete @Builder and Add Builder Pattern

--- ohouse/src/main/java/com/ohouse/ohouse/entity/Menu.java
+++ ohouse/src/main/java/com/ohouse/ohouse/entity/Menu.java
@@ -1,9 +1,6 @@
 package com.ohouse.ohouse.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -11,9 +8,6 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "menu")
 @Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Menu {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -51,4 +45,86 @@ public class Menu {
   @Column(name = "chit_name")
   private String chitName;
 
+  protected Menu() {
+  }
+
+  private Menu(MenuBuilder builder) {
+    this.id = builder.id;
+    this.menuNameKor = builder.menuNameKor;
+    this.descriptionKor = builder.descriptionKor;
+    this.menuNameEng = builder.menuNameEng;
+    this.descriptionEng = builder.descriptionEng;
+    this.menuPrice = builder.menuPrice;
+    this.category = builder.category;
+    this.isAvailable = builder.isAvailable;
+    this.imagePath = builder.imagePath;
+    this.chitName = builder.chitName;
+  }
+
+  public static class MenuBuilder {
+    private Long id;
+    private String menuNameKor;
+    private String descriptionKor;
+    private String menuNameEng;
+    private String descriptionEng;
+    private BigDecimal menuPrice;
+    private Category category;
+    private boolean isAvailable;
+    private String imagePath;
+    private String chitName;
+
+    public MenuBuilder id(Long id) {
+      this.id = id;
+      return this;
+    }
+
+    public MenuBuilder menuNameKor(String menuNameKor) {
+      this.menuNameKor = menuNameKor;
+      return this;
+    }
+
+    public MenuBuilder descriptionKor(String descriptionKor) {
+      this.descriptionKor = descriptionKor;
+      return this;
+    }
+
+    public MenuBuilder menuNameEng(String menuNameEng) {
+      this.menuNameEng = menuNameEng;
+      return this;
+    }
+
+    public MenuBuilder descriptionEng(String descriptionEng) {
+      this.descriptionEng = descriptionEng;
+      return this;
+    }
+
+    public MenuBuilder menuPrice(BigDecimal menuPrice) {
+      this.menuPrice = menuPrice;
+      return this;
+    }
+
+    public MenuBuilder category(Category category) {
+      this.category = category;
+      return this;
+    }
+
+    public MenuBuilder isAvailable(boolean isAvailable) {
+      this.isAvailable = isAvailable;
+      return this;
+    }
+
+    public MenuBuilder imagePath(String imagePath) {
+      this.imagePath = imagePath;
+      return this;
+    }
+
+    public MenuBuilder chitName(String chitName) {
+      this.chitName = chitName;
+      return this;
+    }
+
+    public Menu build() {
+      return new Menu(this);
+    }
+  }
 }
