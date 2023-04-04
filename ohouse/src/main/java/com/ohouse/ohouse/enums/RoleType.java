d3b66a3370e7da0898f5c60f9286ea440b5d26f6
jnk1m <jn7277@hotmail.com>
2023-04-04 14:32:14 +0900

    Remove field. Will use ORDINAL

--- ohouse/src/main/java/com/ohouse/ohouse/enums/RoleType.java
+++ ohouse/src/main/java/com/ohouse/ohouse/enums/RoleType.java
@@ -1,16 +1,7 @@
 package com.ohouse.ohouse.enums;
 
 public enum RoleType {
-  USER(0),
-  ADMIN(1);
+  USER, //Ordinal: 0
+  ADMIN; //Ordinal: 1
 
-  private final int roleNum;
-
-  RoleType(int roleNum) {
-    this.roleNum = roleNum;
-  }
-
-  public int getRoleNum() {
-    return roleNum;
-  }
 }
