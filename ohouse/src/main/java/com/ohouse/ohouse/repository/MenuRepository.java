dfc3e85281ba728755b1c109c35904ac87d401ff
jnk1m <jn7277@hotmail.com>
2023-04-12 22:26:13 +0900

    Create findByCategoryAndIsAvailableTrue

--- ohouse/src/main/java/com/ohouse/ohouse/repository/MenuRepository.java
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/MenuRepository.java
@@ -1,9 +1,18 @@
 package com.ohouse.ohouse.repository;
 
+import com.ohouse.ohouse.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ohouse.ohouse.entity.Menu;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+  @Query("SELECT m FROM Menu m WHERE m.category = :category AND m.isAvailable = true")
+  List<Menu> findByCategoryAndIsAvailableTrue(@Param("category") Category categoryId);
+
 }
