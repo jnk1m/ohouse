b66b61e01c3c71ffb6354e4b2d1be846b6a1c686
jnk1m <jn7277@hotmail.com>
2023-04-12 22:25:33 +0900

    Create MenuDTO

---
+++ ohouse/src/main/java/com/ohouse/ohouse/domain/MenuDTO.java
@@ -0,0 +1,34 @@
+package com.ohouse.ohouse.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class MenuDTO {
+  private Long menuId;
+
+  private String menuNameEng;
+
+  private String descriptionEng;
+
+  private String menuNameKor;
+
+  private String descriptionKor;
+
+  private BigDecimal menuPrice;
+
+  private String imagePath;
+
+  public MenuDTO(Long menuId, String menuNameEng, String descriptionEng, String menuNameKor, String descriptionKor, BigDecimal menuPrice, String imagePath) {
+    this.menuId = menuId;
+    this.menuNameEng = menuNameEng;
+    this.descriptionEng = descriptionEng;
+    this.menuNameKor = menuNameKor;
+    this.descriptionKor = descriptionKor;
+    this.menuPrice = menuPrice;
+    this.imagePath = imagePath;
+  }
+}
