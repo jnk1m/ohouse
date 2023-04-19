f0f2a83375c2b510a31f46a9c949f6a8b49a63fd
jnk1m <jn7277@hotmail.com>
2023-04-19 14:51:34 +0900

    Create GoogleOAuth2Properties

---
+++ ohouse/src/main/java/com/ohouse/ohouse/config/GoogleOAuth2Properties.java
@@ -0,0 +1,24 @@
+package com.ohouse.ohouse.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class GoogleOAuth2Properties {
+
+  @Value("${spring.security.oauth2.client.registration.google.client-id}")
+  private String clientId;
+
+  @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+  private String clientSecret;
+
+  @Value("${spring.security.oauth2.client.registration.google.scope}")
+  private String scope;
+
+  @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+  private String redirectUri;
+
+
+}
