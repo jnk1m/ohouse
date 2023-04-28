81413447cbde49daa6437a922cc110d3a63e0c97
jnk1m <jn7277@hotmail.com>
2023-04-28 15:57:11 +0900

    Delete PostMapping

--- ohouse/src/main/java/com/ohouse/ohouse/controller/menuController.java
+++ ohouse/src/main/java/com/ohouse/ohouse/controller/menuController.java
@@ -7,11 +7,13 @@ import com.ohouse.ohouse.service.CategoryService;
 import com.ohouse.ohouse.service.MenuService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,11 +26,16 @@ public class MenuController {
   private final MenuService menuService;
   private final CategoryService categoryService;
 
-  @PostMapping()
-  @ResponseBody
-  public List<MenuDTO> getMenuList(@RequestParam Long categoryId) {
-
+  @GetMapping("/{categoryId}")
+  public String getMenus(@PathVariable Long categoryId, Model model) {
     Category category = categoryService.findCategory(categoryId);
+
+    /*Category ID 1 to 11 are valid menu IDs, but 12 and above are reserved for options.
+    Therefore, when customers request the list of available menus, only the ones with IDs between 1 and 11 should be returned.
+    Any ID higher than 11 should result in an error message.*/
+    if (categoryId > 11) {
+      throw new InvalidCategoryIdException("Invalid Category");
+    }
 
     List<Menu> menuList = menuService.getMenusInCategory(category);
     List<MenuDTO> menuDTOList = new ArrayList<>();
@@ -39,8 +46,17 @@ public class MenuController {
       menuDTOList.add(menuDTO);
     }
 
-    return menuDTOList;
+    model.addAttribute("category", category);
+    model.addAttribute("menus", menuDTOList);
+    return "menu-list";
   }
-
-
 }
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class InvalidCategoryIdException extends RuntimeException {
+  public InvalidCategoryIdException(String message) {
+    super(message);
+  }
+}
+
+
