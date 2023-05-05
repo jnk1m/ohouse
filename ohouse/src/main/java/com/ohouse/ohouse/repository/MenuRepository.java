73caead13b665bf9c87fc66099122273bff5954d
jnk1m <jn7277@hotmail.com>
2023-05-05 16:14:29 +0900

    feat: Add queries to retrieve menu and options

--- ohouse/src/main/java/com/ohouse/ohouse/repository/MenuRepository.java
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/MenuRepository.java
@@ -1,9 +1,9 @@
 package com.ohouse.ohouse.repository;
 
+import com.ohouse.ohouse.domain.MenuOptionDTO;
 import com.ohouse.ohouse.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ohouse.ohouse.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,5 +14,15 @@ public interface MenuRepository extends JpaRepository<Menu, Long> {
 
   @Query("SELECT m FROM Menu m WHERE m.category = :category AND m.isAvailable = true")
   List<Menu> findByCategoryAndIsAvailableTrue(@Param("category") Category categoryId);
+
+  @Query("SELECT m FROM Menu m WHERE m.menuId = :menuId AND m.isAvailable = true")
+  Menu findByMenuIdAndIsAvailableTrue(@Param("menuId") Long menuId);
+
+  @Query("SELECT new com.ohouse.ohouse.domain.MenuOptionDTO(c.categoryName, o.optionName) " +
+          "FROM MenuOption mo " +
+          "JOIN mo.category c " +
+          "JOIN mo.options o " +
+          "WHERE mo.menu = :menu")
+  List<MenuOptionDTO> findByMenuId(@Param("menu") Menu menu);
 
 }
