1f89f9f1ae5ac6e74a2097b03be95df93c40efbb
jnk1m <jn7277@hotmail.com>
2023-05-05 16:15:07 +0900

    feat: Add queries to retrieve menu and options

--- ohouse/src/main/java/com/ohouse/ohouse/service/MenuService.java
+++ ohouse/src/main/java/com/ohouse/ohouse/service/MenuService.java
@@ -1,10 +1,20 @@
 package com.ohouse.ohouse.service;
 
+import com.ohouse.ohouse.domain.MenuDTO;
+import com.ohouse.ohouse.domain.MenuOptionDTO;
 import com.ohouse.ohouse.entity.Category;
 import com.ohouse.ohouse.entity.Menu;
 
 import java.util.List;
+import java.util.Map;
 
 public interface MenuService {
-  List<Menu> getMenusInCategory(Category category);
+  List<MenuDTO> getMenusInCategory(Category category);
+
+  MenuDTO getMenuDTO(Long menuId);
+
+  Menu getMenu(Long menuId);
+
+  Map<String, List<MenuOptionDTO>> getMenuOptions(Menu menu);
+
 }
--- ohouse/src/main/java/com/ohouse/ohouse/service/MenuServiceImpl.java
+++ ohouse/src/main/java/com/ohouse/ohouse/service/MenuServiceImpl.java
@@ -1,12 +1,17 @@
 package com.ohouse.ohouse.service;
 
+import com.ohouse.ohouse.domain.MenuDTO;
+import com.ohouse.ohouse.domain.MenuOptionDTO;
 import com.ohouse.ohouse.entity.Category;
 import com.ohouse.ohouse.entity.Menu;
 import com.ohouse.ohouse.repository.MenuRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +19,39 @@ public class MenuServiceImpl implements MenuService{
   private final MenuRepository menuRepository;
 
   @Override
-  public List<Menu> getMenusInCategory(Category category) {
-    return menuRepository.findByCategoryAndIsAvailableTrue(category);
+  public List<MenuDTO> getMenusInCategory(Category category) {
+    List<Menu> menuList = menuRepository.findByCategoryAndIsAvailableTrue(category);
+    List<MenuDTO> menuDTOList = new ArrayList<>();
+
+    for (Menu menu : menuList) {
+      MenuDTO menuDTO = new MenuDTO(menu.getMenuId(), menu.getMenuNameEng(), menu.getDescriptionEng(),
+              menu.getMenuNameKor(), menu.getDescriptionKor(), menu.getMenuPrice(), menu.getImagePath());
+      menuDTOList.add(menuDTO);
+    }
+
+    return menuDTOList;
   }
+
+  @Override
+  public Menu getMenu(Long menuId) {
+    return menuRepository.findByMenuIdAndIsAvailableTrue(menuId);
+  }
+
+  @Override
+  public MenuDTO getMenuDTO(Long menuId) {
+    Menu menu = menuRepository.findByMenuIdAndIsAvailableTrue(menuId);
+
+    return new MenuDTO(menu.getMenuId(), menu.getMenuNameEng(), menu.getDescriptionEng(),
+            menu.getMenuNameKor(), menu.getDescriptionKor(), menu.getMenuPrice(), menu.getImagePath());
+  }
+
+  @Override
+  public Map<String, List<MenuOptionDTO>> getMenuOptions(Menu menu) {
+    List<MenuOptionDTO> menuOptionDTOList = menuRepository.findOptionByMenuId(menu);
+
+    return menuOptionDTOList.stream()
+            .collect(Collectors.groupingBy(MenuOptionDTO::getCategoryName));
+
+  }
+
 }
