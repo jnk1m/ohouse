95a606b16cff778c4fa2d9e334a9a997a0ad4ef1
jnk1m <jn7277@hotmail.com>
2023-05-10 14:44:18 +0900

    feat: Create InvalidCategoryException

---
+++ ohouse/src/main/java/com/ohouse/ohouse/exception/InvalidCategoryIdException.java
@@ -0,0 +1,11 @@
+package com.ohouse.ohouse.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class InvalidCategoryIdException extends RuntimeException {
+  public InvalidCategoryIdException(String message) {
+    super(message);
+  }
+}
