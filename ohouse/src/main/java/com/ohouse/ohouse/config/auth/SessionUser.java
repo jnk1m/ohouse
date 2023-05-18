7497614f6636ef47d4892408ab516d836e7b88d6
jnk1m <jn7277@hotmail.com>
2023-05-18 15:15:01 +0900

    feat: Create Security and OAuth configuration files and add configurations

---
+++ ohouse/src/main/java/com/ohouse/ohouse/config/auth/SessionUser.java
@@ -0,0 +1,15 @@
+package com.ohouse.ohouse.config.auth;
+
+import com.ohouse.ohouse.entity.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+  private String name;
+  private String email;
+
+  public SessionUser(User user) {
+    this.name = user.getUserName();
+    this.email = user.getEmail();
+  }
+}
