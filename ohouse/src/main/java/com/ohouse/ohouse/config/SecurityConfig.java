5d777171504417748858e9c3fa23dde35a682a95
jnk1m <jn7277@hotmail.com>
2023-05-18 15:15:28 +0900

    refactor: Modify security configuration

--- ohouse/src/main/java/com/ohouse/ohouse/config/SecurityConfig.java
+++ ohouse/src/main/java/com/ohouse/ohouse/config/SecurityConfig.java
@@ -1,33 +1,35 @@
 package com.ohouse.ohouse.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.ohouse.ohouse.config.auth.CustomOAuth2UserService;
+import com.ohouse.ohouse.enums.Role;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+//@Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  private final CustomOAuth2UserService customOAuth2UserService;
 
-
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
     http
-            .authorizeHttpRequests((authz) -> authz
-                    .antMatchers("/**").permitAll()
-                    .anyRequest().permitAll()
-            )
-            .formLogin()
-            .and()
-            .httpBasic()
-            .and()
             .csrf().disable()
-            .anonymous()
-            .authorities("ROLE_ANONYMOUS");
-
-    return http.build();
-
+            .headers().frameOptions().disable()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/", "/menus/**", "/css/**", "/js/**", "/images/**").permitAll()
+            .antMatchers("/mypage").hasRole(Role.USER.name())
+            .anyRequest().authenticated()
+            .and()
+            .logout()
+            .logoutSuccessUrl("/")
+            .and()
+            .oauth2Login()
+            .userInfoEndpoint()
+            .userService(customOAuth2UserService);
   }
 
 
