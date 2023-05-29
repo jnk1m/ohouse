943384b4492954aca4ce23821b09e203ea938b99
jnk1m <jn7277@hotmail.com>
2023-05-29 14:56:14 +0900

    feat: Create sendVerificationCode method

--- ohouse/src/main/java/com/ohouse/ohouse/controller/UserController.java
+++ ohouse/src/main/java/com/ohouse/ohouse/controller/UserController.java
@@ -2,21 +2,28 @@ package com.ohouse.ohouse.controller;
 
 import com.ohouse.ohouse.domain.UserDTO;
 import com.ohouse.ohouse.security.auth.SessionUser;
+import com.ohouse.ohouse.service.PhoneValidationService;
 import com.ohouse.ohouse.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/account")
 public class UserController {
 
   private final UserService userService;
+  private final PhoneValidationService phoneValidationService;
 
-  @GetMapping("/account")
+  @GetMapping()
   public String getAccountPage(Model model, HttpSession session) {
     SessionUser user = (SessionUser) session.getAttribute("user");
 
@@ -26,4 +33,12 @@ public class UserController {
 
     return "/mypage";
   }
+
+  @PostMapping("/sendVerificationCode")
+  public void sendVerificationCode(@RequestParam String phoneNumber){
+    phoneValidationService.sendVerificationCode(phoneNumber);
+  }
+
+
+
 }
