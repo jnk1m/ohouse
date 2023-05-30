cdd5ac92d8224a9d147e0c7ad8bd463b13931e45
jnk1m <jn7277@hotmail.com>
2023-05-30 15:04:02 +0900

    refactor: Remove service SID and modify message creator

--- ohouse/src/main/java/com/ohouse/ohouse/service/PhoneValidationService.java
+++ ohouse/src/main/java/com/ohouse/ohouse/service/PhoneValidationService.java
@@ -10,34 +10,21 @@ import org.springframework.stereotype.Service;
 public class PhoneValidationService {
   private final String ACCOUNT_SID;
   private final String AUTH_TOKEN;
-  private final String VERIFY_SERVICE_ID;
 
   public PhoneValidationService(@Value("${twilio.account_sid}") String account_sid,
-                                @Value("${twilio.auth_token}") String auth_token,
-                                @Value("${twilio.verify_service_id}") String verify_service_id) {
+                                @Value("${twilio.auth_token}") String auth_token) {
     this.ACCOUNT_SID = account_sid;
     this.AUTH_TOKEN = auth_token;
-    this.VERIFY_SERVICE_ID = verify_service_id;
 
     Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
   }
 
   public void sendVerificationCode(String phoneNumber) {
-//    Verification verification = Verification.creator(
-//            VERIFY_SERVICE_ID,
-//            phoneNumber,
-//            "sms"
-//    ).create();
-//    System.out.println("Sent verification: "+ verification.getSid());
-
-    Message message = Message
-            .creator(
-                    new PhoneNumber("+15558675309"),
+    Message message = Message.creator(
                     new PhoneNumber(phoneNumber),
-                    "Ohouse Message Test"
-            )
+                    new PhoneNumber("+"),
+                    "Where's Wallace?")
             .create();
-
     System.out.println(message.getSid());
   }
 
