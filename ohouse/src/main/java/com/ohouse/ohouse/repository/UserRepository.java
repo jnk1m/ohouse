8ea43aff5a6b742b62c286792723cf78010034c3
jnk1m <jn7277@hotmail.com>
2023-05-31 17:40:50 +0900

    feat: Add DTO projection query DSL

--- ohouse/src/main/java/com/ohouse/ohouse/repository/UserRepository.java
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/UserRepository.java
@@ -1,10 +1,17 @@
 package com.ohouse.ohouse.repository;
 
+import com.ohouse.ohouse.domain.UserDTO;
 import com.ohouse.ohouse.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
   Optional<User> findByEmail(String email);
+
+  @Query("SELECT new com.ohouse.ohouse.domain.UserDTO(u.userName, u.phoneNumber, u.email, u.isPhoneVerified) " +
+          "FROM User u " +
+          "WHERE u.email = :email")
+  Optional<UserDTO> findByEmailToDTO (String email);
 }
