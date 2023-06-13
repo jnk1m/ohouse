4f4def45314b1eb3396ada93f78b612c4dbbbb92
jnk1m <jn7277@hotmail.com>
2023-06-13 15:38:14 +0900

    refactor: Modify LettuceConnectionFactory code

--- ohouse/src/main/java/com/ohouse/ohouse/config/RedisConfig.java
+++ ohouse/src/main/java/com/ohouse/ohouse/config/RedisConfig.java
@@ -1,12 +1,16 @@
 package com.ohouse.ohouse.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.protocol.ProtocolVersion;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -14,17 +18,25 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 @Configuration
 public class RedisConfig {
   @Value("${spring.redis.host}")
-  public String host;
+  private String redisHost;
 
   @Value("${spring.redis.port}")
-  public int port;
+  private String redisPort;
+
+  @Value("${spring.redis.password}")
+  private String redisPassword;
 
   @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-    configuration.setHostName(host);
-    configuration.setPort(port);
-    return new LettuceConnectionFactory(configuration);
+  public LettuceConnectionFactory redisConnectionFactory() {
+    RedisStandaloneConfiguration redisStandaloneConfiguration =
+            new RedisStandaloneConfiguration(redisHost, Integer.parseInt(redisPort));
+    redisStandaloneConfiguration.setPassword(RedisPassword.of(redisPassword));
+
+    LettuceClientConfiguration lettuceClientConfiguration = LettuceClientConfiguration.builder()
+            .clientOptions(ClientOptions.builder().protocolVersion(ProtocolVersion.RESP2).build())
+            .build();
+
+    return new LettuceConnectionFactory(redisStandaloneConfiguration, lettuceClientConfiguration);
   }
 
   @Bean
@@ -35,6 +47,7 @@ public class RedisConfig {
   public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
     RedisTemplate<Object, Object> template = new RedisTemplate<>();
     template.setConnectionFactory(redisConnectionFactory);
+
     return template;
   }
 
@@ -44,6 +57,5 @@ public class RedisConfig {
   public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
     return new StringRedisTemplate(redisConnectionFactory);
   }
-
 
 }
