ae0879691438f9d96c074e52f537b718c4ffa4ca
jnk1m <jn7277@hotmail.com>
2023-06-22 16:43:23 +0900

    feat: Add try-catch block to sendVerificationCode, complete checkVerificationCode

--- ohouse/src/main/java/com/ohouse/ohouse/controller/UserController.java
+++ ohouse/src/main/java/com/ohouse/ohouse/controller/UserController.java
@@ -5,6 +5,8 @@ import com.ohouse.ohouse.security.auth.SessionUser;
 import com.ohouse.ohouse.service.PhoneValidationService;
 import com.ohouse.ohouse.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,29 +22,43 @@ public class UserController {
   private final UserService userService;
   private final PhoneValidationService phoneValidationService;
 
-  @GetMapping()
-  public String getAccountPage(Model model, HttpSession session) {
+  @ModelAttribute("userDTO")
+  public UserDTO getUserDTO(HttpSession session) {
     SessionUser user = (SessionUser) session.getAttribute("user");
 
-    UserDTO userDTO = userService.getUserByEmail(user.getEmail());
+    return userService.getUserByEmail(user.getEmail());
+  }
 
+  @GetMapping()
+  public String getAccountPage(Model model, @ModelAttribute("userDTO") UserDTO userDTO) {
     model.addAttribute("user", userDTO);
 
     return "mypage";
   }
 
   @PostMapping("/verification-codes")
-  public String sendVerificationCode(@RequestBody Map<String, String> payload) {
-    phoneValidationService.sendVerification(payload.get("phoneNumber"));
-    return "redirect:/accounts";
+  public ResponseEntity<?> sendVerificationCode(@RequestBody Map<String, String> payload) {
+    try {
+      phoneValidationService.sendVerification(payload.get("phoneNumber"));
+
+      return new ResponseEntity<>("{\"message\": \"Verification code sent.\"}", HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>("{\"message\": \"Error while sending verification code.\"}", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @PostMapping("/verification-codes/verification")
-  public void checkVerificationCode(@RequestBody Map<String, String> payload){
-    String verificationCheckResult = phoneValidationService.checkVerificationCode(payload.get("phoneNumber"), payload.get("verificationCode"));
+  public ResponseEntity<String> checkVerificationCode(@RequestBody Map<String, String> payload, @ModelAttribute("userDTO") UserDTO userDTO) {
+    String phoneNumber = payload.get("phoneNumber");
 
-    if(verificationCheckResult.equals("approved")){
-      System.out.println("approved");
+    String verificationCheckResult = phoneValidationService.checkVerificationCode(phoneNumber, payload.get("verificationCode"));
+
+    if (verificationCheckResult.equals("approved")) {
+      userService.savePhoneNumberAndMarkVerified(userDTO.getUserId(), phoneNumber);
+
+      return new ResponseEntity<>("{\"message\": \"Verification successful\"}", HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>("{\"message\": \"Verification failed\"}", HttpStatus.BAD_REQUEST);
     }
   }
 
