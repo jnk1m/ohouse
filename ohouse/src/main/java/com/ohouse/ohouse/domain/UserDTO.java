45416770f7ac35b87071e2aec516cb88085adfe4
jnk1m <jn7277@hotmail.com>
2023-06-23 15:53:38 +0900

    refactor: Implement Serializable

--- ohouse/src/main/java/com/ohouse/ohouse/domain/UserDTO.java
+++ ohouse/src/main/java/com/ohouse/ohouse/domain/UserDTO.java
@@ -3,9 +3,13 @@ package com.ohouse.ohouse.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class UserDTO {
+public class UserDTO implements Serializable {
+  //private static final long serialVersionUID = 1L;
+
   private Long userId;
 
   private String userName;
