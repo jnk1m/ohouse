fd9c152d8112ee3e85909eb1ce3c11dfbfaee26f
jnk1m <jn7277@hotmail.com>
2023-07-11 17:02:40 +0900

    feat: Create repository

---
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/CartOptionRepository.java
@@ -0,0 +1,7 @@
+package com.ohouse.ohouse.repository;
+
+import com.ohouse.ohouse.entity.CartOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartOptionRepository extends JpaRepository<CartOption, Integer> {
+}
---
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/OptionRepository.java
@@ -0,0 +1,7 @@
+package com.ohouse.ohouse.repository;
+
+import com.ohouse.ohouse.entity.Options;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionRepository extends JpaRepository<Options,Integer> {
+}
