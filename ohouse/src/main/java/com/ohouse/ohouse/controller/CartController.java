be7c69ca9ba7e8bb10f9536a82ce3df7ed3e9d0b
jnk1m <jn7277@hotmail.com>
2023-07-12 23:53:11 +0900

    feat: Implement getCartList method and modify repetitive cart builder in addItemToCart

--- ohouse/src/main/java/com/ohouse/ohouse/controller/CartController.java
+++ ohouse/src/main/java/com/ohouse/ohouse/controller/CartController.java
@@ -1,5 +1,6 @@
 package com.ohouse.ohouse.controller;
 
+import com.ohouse.ohouse.domain.UserCartDTO;
 import com.ohouse.ohouse.domain.UserDTO;
 import com.ohouse.ohouse.entity.Cart;
 import com.ohouse.ohouse.entity.CartOption;
@@ -10,9 +11,8 @@ import com.ohouse.ohouse.service.OptionService;
 import com.ohouse.ohouse.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -27,6 +27,15 @@ public class CartController {
   private final UserService userService;
   private final OptionService optionService;
 
+  @GetMapping
+  public String getCartList(HttpSession session, Model model){
+    UserDTO userDTO = (UserDTO) session.getAttribute("user");
+    List<UserCartDTO> cartList = cartService.getCartList(userDTO.getUserId());
+
+    model.addAttribute("cartList",cartList);
+    return "carts";
+  }
+
   @PostMapping
   public String addItemToCart(@RequestParam(value = "menuOption", required = false) List<Integer> optionId,
                               @RequestParam("quantity") int quantity,
@@ -35,33 +44,22 @@ public class CartController {
   ) throws MenuNotFoundException {
     UserDTO userDTO = (UserDTO) session.getAttribute("user");
 
+    Cart cart = Cart.builder()
+            .quantity(quantity)
+            .menu(menuService.getMenu(menuId))
+            .user(userService.getUserById(userDTO.getUserId()))
+            .build();
+    Cart createdCart = cartService.createCart(cart);
+
     if (optionId != null) {
-      Cart cart = Cart.builder()
-              .quantity(quantity)
-              .menu(menuService.getMenu(menuId))
-              .user(userService.getUserById(userDTO.getUserId()))
-              .build();
-
-      Cart createdCart = cartService.createCart(cart);
-
       List<CartOption> cartOptionList = optionId.stream().map(id -> CartOption.builder().cart(createdCart)
               .option(optionService.getById(id)).build()).collect(Collectors.toList());
 
       List<CartOption> cartOption = cartService.createCartOption(cartOptionList);
-
-    } else {
-      Cart cart = Cart.builder()
-              .quantity(quantity)
-              .menu(menuService.getMenu(menuId))
-              .user(userService.getUserById(userDTO.getUserId()))
-              .build();
-
-      Cart createdCart = cartService.createCart(cart);
     }
 
     //TODO: responseEntity로 리턴하도록 수정하기. 전화번호 인증처럼 특정 메세지 받으면 장바구니 담기 완료했다는 팝업 띄워주기
     return "index";
-
   }
 
 
