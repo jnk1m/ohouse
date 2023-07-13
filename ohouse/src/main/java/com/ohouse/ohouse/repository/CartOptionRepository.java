845e9a81c641a6c949bef40e904e985e6a59168a
jnk1m <jn7277@hotmail.com>
2023-07-13 17:15:23 +0900

    feat: Create JPQL query for findOptionByCartId

--- ohouse/src/main/java/com/ohouse/ohouse/repository/CartOptionRepository.java
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/CartOptionRepository.java
@@ -1,7 +1,19 @@
 package com.ohouse.ohouse.repository;
 
+import com.ohouse.ohouse.domain.CartOptionDTO;
 import com.ohouse.ohouse.entity.CartOption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CartOptionRepository extends JpaRepository<CartOption, Integer> {
+
+  @Query("SELECT new com.ohouse.ohouse.domain.CartOptionDTO(" +
+          "co.cart.cartId, co.option.optionId, o.optionName, o.optionPrice) " +
+          "FROM CartOption co " +
+          "JOIN co.option o " +
+          "WHERE co.cart.cartId = :parentCartId")
+  List<CartOptionDTO> findOptionByCartId(int parentCartId);
 }
+
