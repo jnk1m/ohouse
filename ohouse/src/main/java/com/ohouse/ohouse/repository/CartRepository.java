a518584200acfeefd6c682a2968c93932d864bd5
jnk1m <jn7277@hotmail.com>
2023-07-13 17:14:33 +0900

    feat: Create JPQL query for findCartItemsByUserId

--- ohouse/src/main/java/com/ohouse/ohouse/repository/CartRepository.java
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/CartRepository.java
@@ -1,8 +1,20 @@
 package com.ohouse.ohouse.repository;
 
+import com.ohouse.ohouse.domain.CartItemDTO;
 import com.ohouse.ohouse.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CartRepository extends JpaRepository<Cart, Integer> {
+
+  @Query("SELECT new com.ohouse.ohouse.domain.CartItemDTO(" +
+          "cart.cartId, cart.quantity, cart.menu.menuId, menu.menuNameEng, menu.menuPrice) " +
+          "FROM Cart cart " +
+          "JOIN cart.menu menu " +
+          "WHERE cart.user.userId = :userId")
+  List<CartItemDTO> findCartItemsByUserId(int userId);
+
 
 }
