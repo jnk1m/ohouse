a51e172fd6975dcc003b2a05410f31fd437ae99f
jnk1m <jn7277@hotmail.com>
2023-07-14 14:57:50 +0900

    chore: Change CSRF configuration

--- ohouse/src/main/java/com/ohouse/ohouse/config/SecurityConfig.java
+++ ohouse/src/main/java/com/ohouse/ohouse/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -21,7 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .invalidSessionUrl("/")
             .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
             .and()
-            .csrf().disable()
+            .csrf()
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .and()
             .headers().frameOptions().disable()
             .and()
             .authorizeRequests()
