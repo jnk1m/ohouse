f685f4cb6bcfcf3d726b071eadbdda71c6b5fa18
jnk1m <jn7277@hotmail.com>
2023-08-01 15:31:24 +0900

    feat: Create placeOrder method for transaction handling

--- ohouse/src/main/java/com/ohouse/ohouse/service/OrderService.java
+++ ohouse/src/main/java/com/ohouse/ohouse/service/OrderService.java
@@ -2,7 +2,11 @@ package com.ohouse.ohouse.service;
 
 import com.ohouse.ohouse.entity.Order;
 
+import java.util.List;
+
 public interface OrderService {
 
   Order createOrder(Order order);
+
+  Order placeOrder(Order newOrder, List<Integer> usersCartId);
 }
