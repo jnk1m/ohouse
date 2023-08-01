afcb5708b6fcbd5b6304cef335845990b71bf28d
jnk1m <jn7277@hotmail.com>
2023-08-01 15:26:34 +0900

    feat: Create OrderItemOptionRepository

---
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/OrderItemOptionRepository.java
@@ -0,0 +1,7 @@
+package com.ohouse.ohouse.repository;
+
+import com.ohouse.ohouse.entity.OrderItemOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemOptionRepository extends JpaRepository<OrderItemOption, Integer> {
+}
