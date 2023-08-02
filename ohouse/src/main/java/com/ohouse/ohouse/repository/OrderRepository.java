91c0b16823b6da15ab771f3f1e4102986dccba05
jnk1m <jn7277@hotmail.com>
2023-08-02 15:33:14 +0900

    feat: Create findOrdersByUser

--- ohouse/src/main/java/com/ohouse/ohouse/repository/OrderRepository.java
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/OrderRepository.java
@@ -1,7 +1,16 @@
 package com.ohouse.ohouse.repository;
 
+import com.ohouse.ohouse.domain.OrderListDTO;
 import com.ohouse.ohouse.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+  @Query("SELECT new com.ohouse.ohouse.domain.OrderListDTO(" +
+          "order.orderNumber, order.orderDate, order.orderTimezone, order.orderStatus, order.price) " +
+          "FROM Order order " +
+          "WHERE order.user.userId = :userId")
+  List<OrderListDTO> findOrdersByUser(int userId);
 }
