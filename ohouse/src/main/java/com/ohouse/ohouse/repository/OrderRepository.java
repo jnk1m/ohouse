1bf6bf4a7355c940d7f2a705f9fd5867545be41f
jnk1m <jn7277@hotmail.com>
2023-08-09 15:11:55 +0900

    refactor: Rename for appropriate use of DTO

--- ohouse/src/main/java/com/ohouse/ohouse/repository/OrderRepository.java
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/OrderRepository.java
@@ -1,7 +1,7 @@
 package com.ohouse.ohouse.repository;
 
 import com.ohouse.ohouse.domain.OrderDetailDTO;
-import com.ohouse.ohouse.domain.OrderListDTO;
+import com.ohouse.ohouse.domain.OrderSummaryDTO;
 import com.ohouse.ohouse.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,11 +10,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
-  @Query("SELECT new com.ohouse.ohouse.domain.OrderListDTO(" +
+  @Query("SELECT new com.ohouse.ohouse.domain.OrderSummaryDTO(" +
           "order.orderNumber, order.orderDate, order.orderTimezone, order.orderStatus, order.price) " +
           "FROM Order order " +
           "WHERE order.user.userId = :userId")
-  List<OrderListDTO> findOrdersByUser(int userId);
+  List<OrderSummaryDTO> findOrdersByUser(int userId);
 
   @Query("SELECT new com.ohouse.ohouse.domain.OrderDetailDTO("+
   "order.orderId, order.price, order.paymentMethod, order.deliveryAddress, order.deliveryContact, order.specialInstruction, order.orderStatus, order.name, order.orderDate)"+
