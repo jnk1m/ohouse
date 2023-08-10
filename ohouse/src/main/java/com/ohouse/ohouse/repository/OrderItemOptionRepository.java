1bef2f21b3efeef9e7d7aac87c055cb82ff90187
jnk1m <jn7277@hotmail.com>
2023-08-10 23:19:22 +0900

    feat: Implement findOptionByParentOrderId

--- ohouse/src/main/java/com/ohouse/ohouse/repository/OrderItemOptionRepository.java
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/OrderItemOptionRepository.java
@@ -1,7 +1,18 @@
 package com.ohouse.ohouse.repository;
 
+import com.ohouse.ohouse.domain.OrderedItemOptionDTO;
 import com.ohouse.ohouse.entity.OrderItemOption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderItemOptionRepository extends JpaRepository<OrderItemOption, Integer> {
+
+  @Query("SELECT new com.ohouse.ohouse.domain.CartOptionDTO(" +
+          "oio.orderItem.orderItemId, oio.option.optionId, o.optionName, o.optionPrice) " +
+          "FROM OrderItemOption oio " +
+          "JOIN  oio.option o " +
+          "WHERE oio.orderItem.orderItemId = :parentOrderId")
+  List<OrderedItemOptionDTO> findOptionByParentOrderId(int parentOrderId);
 }
