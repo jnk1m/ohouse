a03e025a1c244557e8ddfd98e880be64e7f66a84
jnk1m <jn7277@hotmail.com>
2023-08-10 23:17:31 +0900

    feat: Declare field variables and create constructor

--- ohouse/src/main/java/com/ohouse/ohouse/domain/OrderedItemOptionDTO.java
+++ ohouse/src/main/java/com/ohouse/ohouse/domain/OrderedItemOptionDTO.java
@@ -2,6 +2,8 @@ package com.ohouse.ohouse.domain;
 
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 /**
  * Represents the selected options for an ordered product.
  * An ordered product can have multiple options. It's embedded within the OrderedItemDTO.
@@ -9,5 +11,15 @@ import lombok.Getter;
 
 @Getter
 public class OrderedItemOptionDTO {
+  private final int parentOrderId;
+  private final int optionId;
+  private final String optionName;
+  private final BigDecimal optionPrice;
 
+  public OrderedItemOptionDTO(int parentOrderId, int optionId, String optionName, BigDecimal optionPrice) {
+    this.parentOrderId = parentOrderId;
+    this.optionId = optionId;
+    this.optionName = optionName;
+    this.optionPrice = optionPrice;
+  }
 }
