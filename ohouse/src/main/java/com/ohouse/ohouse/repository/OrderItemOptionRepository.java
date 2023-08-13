2a97fdb4b6af9be6b480a3f416d67521eff755aa
jnk1m <jn7277@hotmail.com>
2023-08-13 17:29:05 +0900

    refactor: Rename variable to orderItemId for clarity

--- ohouse/src/main/java/com/ohouse/ohouse/repository/OrderItemOptionRepository.java
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/OrderItemOptionRepository.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 public interface OrderItemOptionRepository extends JpaRepository<OrderItemOption, Integer> {
 
-  @Query("SELECT new com.ohouse.ohouse.domain.CartOptionDTO(" +
-          "oio.orderItem.orderItemId, oio.option.optionId, o.optionName, o.optionPrice) " +
+  @Query("SELECT new com.ohouse.ohouse.domain.OrderedItemOptionDTO(" +
+          "oio.orderItem.orderItemId, o.optionName, o.optionPrice) " +
           "FROM OrderItemOption oio " +
-          "JOIN  oio.option o " +
-          "WHERE oio.orderItem.orderItemId = :parentOrderId")
-  List<OrderedItemOptionDTO> findOptionByParentOrderId(int parentOrderId);
+          "JOIN  Options o on oio.option.optionId = o.optionId " +
+          "WHERE oio.orderItem.orderItemId = :parentOrderItemId")
+  List<OrderedItemOptionDTO> findOptionByParentOrderItemId(int parentOrderItemId);
 }
