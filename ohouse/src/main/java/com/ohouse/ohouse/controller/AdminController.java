8cced13a2733907c1470a5bd4a5e8c022013d5ad
jnk1m <jn7277@hotmail.com>
2023-08-14 15:22:23 +0900

    feat: Format order time for display and implement methods to change order status

--- ohouse/src/main/java/com/ohouse/ohouse/controller/AdminController.java
+++ ohouse/src/main/java/com/ohouse/ohouse/controller/AdminController.java
@@ -1,14 +1,28 @@
 package com.ohouse.ohouse.controller;
 
 import com.ohouse.ohouse.domain.OrderSummaryDTO;
+import com.ohouse.ohouse.domain.OrderSummaryDisplayDTO;
+import com.ohouse.ohouse.enums.OrderStatus;
 import com.ohouse.ohouse.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,12 +30,61 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
   private final OrderService orderService;
 
+  @GetMapping
+  public String getAdminPage() {
+    return "admin/admin";
+  }
+
   @GetMapping("/orders")
   public String getOrdersPage(Model model, Pageable pageable) {
     Page<OrderSummaryDTO> orders = orderService.getAllOrders(pageable);
 
-    model.addAttribute("orders", orders);
+    List<OrderSummaryDisplayDTO> formattedOrders = orders.stream()
+            .map(order -> {
+              //TODO: Modify to fetch the timezone ID from the client.
+              ZonedDateTime customerDateTime = order.getOrderDate().withZoneSameInstant(ZoneId.of("Asia/Seoul"));
+              String formattedDateTime = customerDateTime.format(DateTimeFormatter.ofPattern("MMMM dd, yyyy HH:mm"));
+              return new OrderSummaryDisplayDTO(order.getOrderNumber(), formattedDateTime, order.getOrderStatus(), order.getOrderPrice());
+            })
+            .collect(Collectors.toList());
+
+    Page<OrderSummaryDisplayDTO> formattedOrdersWithPage = new PageImpl<>(formattedOrders, PageRequest.of(0, formattedOrders.size()), formattedOrders.size());
+
+    model.addAttribute("orders", formattedOrdersWithPage);
 
     return "admin/admin-orders";
   }
+
+  @PostMapping("/confirmation/{orderNumber}")
+  public ResponseEntity<String> confirmOrder(@PathVariable("orderNumber") String orderNumber) {
+    try {
+      orderService.updateOrderStatus(orderNumber, OrderStatus.PROCESSING);
+      return new ResponseEntity<>(HttpStatus.OK);
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  @PostMapping("/cancellation/{orderNumber}")
+  public ResponseEntity<String> cancelOrder(@PathVariable("orderNumber") String orderNumber) {
+    try {
+      orderService.updateOrderStatus(orderNumber, OrderStatus.CANCELLED);
+      return new ResponseEntity<>(HttpStatus.OK);
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  @PostMapping("/completion/{orderNumber}")
+  public ResponseEntity<String> completeOrder(@PathVariable("orderNumber") String orderNumber) {
+    try {
+      orderService.updateOrderStatus(orderNumber, OrderStatus.COMPLETED);
+      //TODO: UPDATE USER ORDER COUNT +1
+      return new ResponseEntity<>(HttpStatus.OK);
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+
 }
