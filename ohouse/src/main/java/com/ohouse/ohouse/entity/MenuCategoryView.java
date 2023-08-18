5422b0c0b8c135fecfa14d52fe6830f46d010ed6
jnk1m <jn7277@hotmail.com>
2023-08-18 15:01:58 +0900

    feat: Create View Entity for Category View Using View

---
+++ ohouse/src/main/java/com/ohouse/ohouse/entity/MenuCategoryView.java
@@ -0,0 +1,24 @@
+package com.ohouse.ohouse.entity;
+
+import com.ohouse.ohouse.enums.CategoryType;
+import lombok.Getter;
+import net.jcip.annotations.Immutable;
+
+import javax.persistence.*;
+
+@Entity
+@Immutable
+@Table(name = "menu_category")
+@Getter
+public class MenuCategoryView {
+  @Id
+  @Column(name = "category_id")
+  private int categoryId;
+
+  @Column(name = "category_name")
+  private String categoryName;
+
+  @Column(name = "type")
+  @Enumerated(EnumType.STRING)
+  private CategoryType categoryType;
+}
