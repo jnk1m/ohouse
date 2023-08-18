4d47def048e22d1b9d795ebbefe4884bc643d6e2
jnk1m <jn7277@hotmail.com>
2023-08-18 15:02:55 +0900

    feat: Create View Repository for Category View Using View

---
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/MenuCategoryViewRepository.java
@@ -0,0 +1,7 @@
+package com.ohouse.ohouse.repository;
+
+import com.ohouse.ohouse.entity.MenuCategoryView;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuCategoryViewRepository extends JpaRepository<MenuCategoryView, Integer> {
+}
---
+++ ohouse/src/main/java/com/ohouse/ohouse/repository/OptionCategoryViewRepository.java
@@ -0,0 +1,7 @@
+package com.ohouse.ohouse.repository;
+
+import com.ohouse.ohouse.entity.OptionCategoryView;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionCategoryViewRepository extends JpaRepository<OptionCategoryView, Integer> {
+}
